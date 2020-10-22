dfc47d199e0c49534a6aa2e7d756cdb0671f93d1
Will Shen <will.shen@anz.com>
2020-10-22 21:04:58 +1100

    finish set with functions - union, difference, intersection and equal

--- pkg/set/set.go
+++ pkg/set/set.go
@@ -2,6 +2,7 @@ package set
 
 import (
 	"fmt"
+	"reflect"
 	"sort"
 )
 
@@ -54,4 +55,65 @@ func (s Set) Has(value interface{}) bool {
 		}
 	}
 	return false
+}
+
+// Subset returns true if s1 is a subset of s2, e.g. all elements of s1 exists in s2
+func Subset(s1, s2 Set) bool {
+	for k := range s1 {
+		if !s2.Has(k) {
+			return false
+		}
+	}
+	return true
+}
+
+// Disjoint return true if there are no common elements between two sets
+func Disjoint(s1, s2 Set) bool {
+	for k := range s1 {
+		if s2.Has(k) {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal return true if s1 and s2 are exactly the same
+func Equal(s1, s2 Set) bool {
+	return reflect.DeepEqual(s1, s2)
+}
+
+// Intersection returns a set with all the common elements between the two sets
+func Intersection(s1, s2 Set) Set {
+	resultSet := NewSet()
+	for key := range s1 {
+		if s2.Has(key) {
+			resultSet[key] = true
+		}
+	}
+	return resultSet
+}
+
+// Difference returns the elements that only exists in the first set
+func Difference(s1, s2 Set) Set {
+	resultSet := NewSet()
+	for key := range s1 {
+		if !s2.Has(key) {
+			resultSet[key] = true
+		}
+	}
+	return resultSet
+}
+
+// Union returns a set that has all the elements in set 1 and set 2
+func Union(s1, s2 Set) Set {
+	unionSet := NewSet()
+	for key := range s1 { // copy s1 into resultSet
+		unionSet[key] = true
+	}
+	for key := range s2 {
+		if !unionSet.Has(key) {
+			unionSet[key] = true
+		}
+	}
+	return unionSet
 }
--- pkg/set/set_test.go
+++ pkg/set/set_test.go
@@ -92,3 +92,225 @@ func TestHas(t *testing.T) {
 		})
 	}
 }
+
+func TestSubset(t *testing.T) {
+	var subsetTestCases = []struct {
+		name     string
+		set1     []interface{}
+		set2     []interface{}
+		expected bool
+	}{
+		{
+			name:     "empty set",
+			set1:     []interface{}{},
+			set2:     []interface{}{},
+			expected: true,
+		},
+		{
+			name:     "s1 is subset of s2",
+			set1:     []interface{}{1},
+			set2:     []interface{}{1, 2, 3},
+			expected: true,
+		},
+		{
+			name:     "s1 is NOT a subset of s2",
+			set1:     []interface{}{1},
+			set2:     []interface{}{2, 3},
+			expected: false,
+		},
+	}
+	for _, tc := range subsetTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			set1 := NewSetFromSlice(tc.set1)
+			set2 := NewSetFromSlice(tc.set2)
+			actual := Subset(set1, set2)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestDisjoint(t *testing.T) {
+	var disjointTestCases = []struct {
+		name     string
+		set1     []interface{}
+		set2     []interface{}
+		expected bool
+	}{
+		{
+			name:     "empty set",
+			set1:     []interface{}{},
+			set2:     []interface{}{},
+			expected: true,
+		},
+		{
+			name:     "No common elements between s1 and s2",
+			set1:     []interface{}{1},
+			set2:     []interface{}{2, 3},
+			expected: true,
+		},
+		{
+			name:     "There is one common element between s1 and s2",
+			set1:     []interface{}{"a"},
+			set2:     []interface{}{"a", "b", "c"},
+			expected: false,
+		},
+	}
+	for _, tc := range disjointTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			set1 := NewSetFromSlice(tc.set1)
+			set2 := NewSetFromSlice(tc.set2)
+			actual := Disjoint(set1, set2)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestEqual(t *testing.T) {
+	var equalTestCases = []struct {
+		name     string
+		set1     []interface{}
+		set2     []interface{}
+		expected bool
+	}{
+		{
+			name:     "empty set are equal",
+			set1:     []interface{}{},
+			set2:     []interface{}{},
+			expected: true,
+		},
+		{
+			name:     "Equal",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{3, 2, 1},
+			expected: true,
+		},
+		{
+			name:     "Not Equal",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{1, 2, 3, 4},
+			expected: false,
+		},
+		{
+			name:     "Two strings set are not equal",
+			set1:     []interface{}{1, 2, 3, 4},
+			set2:     []interface{}{1, 2, 3},
+			expected: false,
+		},
+	}
+	for _, tc := range equalTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			set1 := NewSetFromSlice(tc.set1)
+			set2 := NewSetFromSlice(tc.set2)
+			actual := Equal(set1, set2)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	var intersectionTestCases = []struct {
+		name     string
+		set1     []interface{}
+		set2     []interface{}
+		expected string
+	}{
+		{
+			name:     "empty set have no intersections",
+			set1:     []interface{}{},
+			set2:     []interface{}{},
+			expected: "{}",
+		},
+		{
+			name:     "One common element",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{3, 4, 5},
+			expected: "{3}",
+		},
+		{
+			name:     "No intersection",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{4, 5, 6},
+			expected: "{}",
+		},
+	}
+	for _, tc := range intersectionTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			set1 := NewSetFromSlice(tc.set1)
+			set2 := NewSetFromSlice(tc.set2)
+			actual := Intersection(set1, set2).String()
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestDifference(t *testing.T) {
+	var differenceTestCases = []struct {
+		name     string
+		set1     []interface{}
+		set2     []interface{}
+		expected string
+	}{
+		{
+			name:     "empty set have no difference",
+			set1:     []interface{}{},
+			set2:     []interface{}{},
+			expected: "{}",
+		},
+		{
+			name:     "two different elements",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{3, 4, 5},
+			expected: "{1, 2}",
+		},
+		{
+			name:     "No intersection",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{1, 2},
+			expected: "{3}",
+		},
+	}
+	for _, tc := range differenceTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			set1 := NewSetFromSlice(tc.set1)
+			set2 := NewSetFromSlice(tc.set2)
+			actual := Difference(set1, set2).String()
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestUnion(t *testing.T) {
+	var unionTestCases = []struct {
+		name     string
+		set1     []interface{}
+		set2     []interface{}
+		expected string
+	}{
+		{
+			name:     "empty set have no difference",
+			set1:     []interface{}{},
+			set2:     []interface{}{},
+			expected: "{}",
+		},
+		{
+			name:     "Only one duplicate element",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{3, 4, 5},
+			expected: "{1, 2, 3, 4, 5}",
+		},
+		{
+			name:     "First set covers everything",
+			set1:     []interface{}{1, 2, 3},
+			set2:     []interface{}{1, 2},
+			expected: "{1, 2, 3}",
+		},
+	}
+	for _, tc := range unionTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			set1 := NewSetFromSlice(tc.set1)
+			set2 := NewSetFromSlice(tc.set2)
+			actual := Union(set1, set2).String()
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
